8e30081603fa6ede9770d70d79b1c1b8299ed669
Brijesh Singh <brijessi@gmail.com>
2018-10-29 14:31:54 -0400

    Added the file name

--- files/archive.sh
+++ files/archive.sh
@@ -2,18 +2,19 @@
 
 archive_file_name="/tmp/sas-config-$(date +'%m%d%Y').tar.gz"
 bucket_path="$(date +'%Y')/$(date +'%B')/"
+bucket_name="presidio-sas-config-bucket"
 
-tar -zcvf $archive_file_name /location_config_directory  > /dev/null 2>&1
+tar -zcvf $archive_file_name /home/centos/test  > /dev/null 2>&1
 
 if [ -f $archive_file_name ]; then
-  aws s3 cp $archive_file_name s3://bucket-name/$bucket_path 
-  
+
+  /usr/local/bin/aws s3 cp $archive_file_name s3://$bucket_name/$bucket_path
+
   if [[ $? -eq 0 ]]; then
-	echo "SAS archive file is copied to s3."
-  else 
-  	echo "Error in copying the archive file to S3."
+        echo "SAS archive file is copied to s3."
+  else
+        echo "Error in copying the archive file to S3."
   fi
 else
   echo 'Error in archiving sas configuration.'
 fi
-
