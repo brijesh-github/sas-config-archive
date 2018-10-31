767a7e7eed61e45e5f836ed8df9df0ebde6caf36
Brijesh Singh <brijessi@gmail.com>
2018-10-31 09:03:43 -0400

    changes with bucket name

--- files/archive.sh
+++ files/archive.sh
@@ -2,7 +2,7 @@
 
 archive_file_name="/tmp/sas-config-$(date +'%m%d%Y').tar.gz"
 bucket_path="$(date +'%Y')/$(date +'%B')/"
-bucket_name="presidio-sas-config-bucket"
+bucket_name="aaa-northeast-sas-config-backup"
 
 tar -zcvf $archive_file_name /home/centos/test  > /dev/null 2>&1
 
